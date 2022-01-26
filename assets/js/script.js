38f0eadba7cbe9a967e75ab8bc5d158f722480a5
Steven Kravitz <Steven.Kravitz95@gmail.com>
2022-01-26 17:07:06 -0500

    abandoned randomization logic, began writing questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,35 @@
 var startButton = document.getElementById('startBtn')
 var nextButton = document.getElementById('nextBtn')
 var questionBox = document.getElementById('questionBox')
-let shuffledQuestions, currentQuestionIndex
 var questionElement = document.getElementById('question')
 var answerButtons = document.getElementById('answerBtns')
+
+var questions = [
+    {
+        question: 'What is the current iteration of HTML?',
+        
+        answers: [
+            {text: 'A.) HTML2', correct: false}
+            {text: 'B.) HTML1', correct: false}
+            {text: 'C.) HTML5', correct: true}
+            {text: 'D.) HTML4', correct: false}
+        ]
+    },
+    question: 'The following is an example of an HTML Tag',
+        
+    answers: [
+        {text: 'A.) *div*', correct: false}
+        {text: 'B.) HTML1', correct: false}
+        {text: 'C.) HTML5', correct: true}
+        {text: 'D.) HTML4', correct: false}
+    ]
+{
+        
+    }
+]
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-    startButton.classList.add('hide');
-    // shuffledQuestions = questions.sort(function(){ Math.random() - .5}); Initial version, converted to arrow function
-	shuffledQuestions = questions.sort(() => Math.random() - .5);
 
 }
